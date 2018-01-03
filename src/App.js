5e35b96ef0718ae63fdc286d52b476c016153027
Steven Olson <steveoscaro@gmail.com>
2018-01-02 23:08:23 -0600

    steps into the revit json and displays some geometry data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
-import { Button, DropdownButton } from 'react-bootstrap'
+import { Button } from 'react-bootstrap'
 import { helpers, box_data } from './helpers'
+import Dropdown from 'react-dropdown'
 import './App.css'
 import flux from './flux.png';
 
@@ -75,6 +76,7 @@ class App extends Component {
     if (!(project.id in dataTables)) {
       var dt = helpers.getUser().getDataTable(project.id)
       dataTables[project.id] = { table: dt, handlers: {}, websocketOpen: false }
+      this.setState({ dataTables })
     }
     return dataTables[project.id]
   }
@@ -96,6 +98,23 @@ class App extends Component {
     this.getCells(selectedProject).then((data) => this.setState({ projectCells: data.entities }))
   }
 
+
+  // RENDER FLUX DATA
+  /**
+   * Get a specific project cell (key).
+   */
+  getCell(project, cell) {
+    return this.getDataTable(project).table.getCell(cell.id)
+  }
+
+  /**
+   * Get the value contained in a cell (key).
+   */
+  getValue(project, cell) {
+    return this.getCell(project, cell).fetch()
+  }
+
+
   renderBody() {
     const { isLoggedIn } = this.state
     if(isLoggedIn) {
@@ -110,13 +129,32 @@ class App extends Component {
   }
 
   initViewport() {
-    if(!this.refs.view) { return }
-    // attach the viewport to the #div view
-    const viewport = new window.FluxViewport(this.refs.view)
-    // set up default lighting for the viewport
-    viewport.setupDefaultLighting()
-    viewport.setGeometryEntity(box_data)
-    viewport.setClearColor(0xffffff)
+    const { projects, projectCells } = this.state
+    if(!this.refs.view || !projectCells) { return }
+    // Could be tracking a selected project in state here insead of using 0 index.
+    this.getValue(projects.entities[0], projectCells[0]).then((data) => {
+      const viewport = new window.FluxViewport(this.refs.view)
+      viewport.setupDefaultLighting()
+      viewport.setClearColor(0xffffff)
+      // if(!data) { viewport.setGeometryEntity(box_data) }
+      if(!data) { return null }
+      let known = window.FluxViewport.isKnownGeom(data.value[0].geometryParameters.geometry[0])
+      console.log('Known Geo data? ', known)
+      if(data && known) { viewport.setGeometryEntity(data.value[0].geometryParameters.geometry[0]) }
+      if(data && !known) { viewport.setGeometryEntity(box_data) }
+      // viewport.setGeometryEntity(box_data)
+    })
+  }
+
+  renderDropDown() {
+    const { projectCells } = this.state
+    if(!projectCells) { return }
+    const options = projectCells.map((cell) => {
+      return {value: cell.id, label: cell.label}
+    })
+    return (
+      <Dropdown options={options} onChange={() => {}} placeholder="Select a cell" />
+    )
   }
 
   render() {
@@ -125,6 +163,7 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Flux Exercise</h1>
           <div className="Spacer" />
+          {this.renderDropDown()}
           {this.renderLoginLogout()}
         </header>
         <div className="App-body" ref='carl1'>
