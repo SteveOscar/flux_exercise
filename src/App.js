fecee26b7c29ba1955e1fc0fa77d6d900dcb7342
Steven Olson <steveoscaro@gmail.com>
2018-01-03 21:34:39 -0600

    refactors code into a more logical order, cleans up un-needed code

--- src/App.js
+++ src/App.js
@@ -1,44 +1,43 @@
 import React, { Component } from 'react'
-import { helpers, box_data } from './helpers'
+import { helpers } from './helpers'
 import Dropdown from 'react-dropdown'
 import './App.css'
 import flux from './flux.png';
 
+const initialStateObj = {
+  isLoggedIn: false,
+  selectedCell: null,
+  selectedProject: null,
+  userProjects: null,
+  dataTables: {}
+}
+
 class App extends Component {
   constructor(props) {
     super(props)
-    this.state = {
-      isLoggedIn: false,
-      projects: null,
-      projectCells: null,
-      selectedCell: null,
-      selectedProject: null,
-      dataTables: {}
-    }
-  }
-
-  componentDidUpdate() {
-    const { projects } = this.state
-    if(this.state.isLoggedIn) {
-      this.initViewport()
-      if(!projects) { this.setProjects() }
-    }
+    this.state = initialStateObj
   }
 
   componentWillMount() {
     helpers.storeFluxUser()
       .then(() => helpers.isLoggedIn().then((res) => {
-        console.log('logged in (mount)? ', res)
         this.setState({ isLoggedIn: res })
       })
     )
   }
 
+  componentDidUpdate() {
+    const { userProjects, isLoggedIn } = this.state
+    if(isLoggedIn) {
+      this.initViewport()
+      if(!userProjects) { this.setProjects() }
+    }
+  }
+
   async setProjects() {
     const user = await helpers.getUser()
-    const projects = await user.listProjects()
-    this.fetchCells(projects)
-    this.setState({ projects: projects.entities })
+    const userProjects = await user.listProjects()
+    this.setState({ userProjects: userProjects.entities })
   }
 
   loginUser() {
@@ -47,7 +46,98 @@ class App extends Component {
 
   logoutUser() {
     helpers.logout()
-    this.setState({ isLoggedIn: false, selectedCell: null, selectedProject: null, projects: null, dataTables: {} })
+    this.setState(initialStateObj)
+  }
+
+  getDataTable(project) {
+    let { dataTables } = this.state
+    if (!(project.id in dataTables)) {
+      var dt = helpers.getUser().getDataTable(project.id)
+      dataTables[project.id] = { table: dt, handlers: {}, websocketOpen: false }
+      this.setState({ dataTables })
+    }
+    return dataTables[project.id]
+  }
+
+  getCells(project) {
+    return this.getDataTable(project).table.listCells()
+  }
+
+  fetchCells(selection) {
+    const { userProjects } = this.state
+    const selectedProject = userProjects.find((project) => project.id === selection.value)
+    this.getCells(selectedProject).then((data) => this.setState({ projectCells: data.entities }))
+  }
+
+
+  getCell(project, cell) {
+    return this.getDataTable(project).table.getCell(cell.id)
+  }
+
+  getValue(project, cell) {
+    return this.getCell(project, cell).fetch()
+  }
+
+  initViewport() {
+    const { selectedCell, selectedProject } = this.state
+    if(!this.refs.view || !selectedCell || !selectedProject) { return }
+    const viewport = new window.FluxViewport(this.refs.view)
+    viewport.setupDefaultLighting()
+    viewport.setClearColor(0xffffff)
+    this.renderData(viewport)
+  }
+
+  updateSelectedCell(selection) {
+    this.setState({selectedCell: selection})
+  }
+
+  updateSelectedProject(selection) {
+    this.setState({selectedProject: selection})
+    this.fetchCells(selection)
+  }
+
+  renderData(viewport) {
+    const { userProjects, projectCells, selectedCell, selectedProject } = this.state
+    if(selectedCell.value === 'clear_data') {
+      viewport.setGeometryEntity(null)
+      this.setState({ selectedCell: null })
+    } else {
+      const cell = projectCells.find((cell) => cell.id === selectedCell.value)
+      const project = userProjects.find((project) => project.id === selectedProject.value)
+      this.getValue(project, cell).then((data) => {
+        if(!data) { return null }
+        if(window.FluxViewport.isKnownGeom(data.value)) { viewport.setGeometryEntity(data.value) }
+      })
+    }
+  }
+
+  renderCellDropDown() {
+    const { projectCells, selectedCell, selectedProject } = this.state
+    if(!projectCells || !selectedProject) { return (<Dropdown className='Dim' disabled placeholder="select cell" />) }
+    let options = projectCells.map((cell) => { return { value: cell.id, label: cell.label } })
+    if(selectedCell) { options.push({ value: 'clear_data', label: '*reset*'  }) }
+    return (
+      <Dropdown
+        value={selectedCell ? selectedCell.label : null}
+        options={options}
+        onChange={this.updateSelectedCell.bind(this)}
+        placeholder="select cell"
+      />
+    )
+  }
+
+  renderProjectDropDown() {
+    const { userProjects, selectedProject } = this.state
+    if(!userProjects) { return (<Dropdown className='Dim' disabled placeholder="select project" />) }
+    const options = userProjects.map((project) => { return { value: project.id, label: project.name } })
+    return (
+      <Dropdown
+        value={selectedProject ? selectedProject.label : null}
+        options={options}
+        onChange={this.updateSelectedProject.bind(this)}
+        placeholder="select project"
+      />
+    )
   }
 
   renderLoginLogout() {
@@ -65,99 +155,12 @@ class App extends Component {
     }
   }
 
-  getDataTable(project) {
-    let { dataTables } = this.state
-    if (!(project.id in dataTables)) {
-      var dt = helpers.getUser().getDataTable(project.id)
-      dataTables[project.id] = { table: dt, handlers: {}, websocketOpen: false }
-      this.setState({ dataTables })
-    }
-    return dataTables[project.id]
-  }
-
-  getCells(project) {
-    return this.getDataTable(project).table.listCells()
-  }
-
-  fetchCells(projects) {
-    if(!projects) { return }
-    // get the project's cells (keys) from flux (returns a promise)
-    const selectedProject = projects.entities[0]
-    this.getCells(selectedProject).then((data) => this.setState({ projectCells: data.entities }))
-  }
-
-
-  getCell(project, cell) {
-    return this.getDataTable(project).table.getCell(cell.id)
-  }
-
-  getValue(project, cell) {
-    return this.getCell(project, cell).fetch()
-  }
-
-  initViewport() {
-    const { projects, projectCells, selectedCell, selectedProject } = this.state
-    if(!this.refs.view || !projectCells || !selectedCell || !selectedProject) { return }
-    if(!selectedCell) { return null }
-    const viewport = new window.FluxViewport(this.refs.view)
-    viewport.setupDefaultLighting()
-    viewport.setClearColor(0xffffff)
-    if(selectedCell.value === 'clear_data') {
-      viewport.setGeometryEntity(null)
-      this.setState({ selectedCell: null })
-    } else {
-      const cell = projectCells.find((cell) => cell.id === selectedCell.value)
-      const project = projects.find((project) => project.id === selectedProject.value)
-      this.getValue(project, cell).then((data) => {
-        if(!data) { return null }
-        let known = window.FluxViewport.isKnownGeom(data.value)
-        if(data && known) { viewport.setGeometryEntity(data.value) }
-      })
-    }
-  }
-
-  renderCellDropDown() {
-    const { projectCells, selectedCell, selectedProject } = this.state
-    if(!projectCells || !selectedProject) { return (<Dropdown disabled placeholder="Cell" />) }
-    const options = projectCells.map((cell) => {
-      return { value: cell.id, label: cell.label }
-    })
-    if(selectedCell) { options.push({ value: 'clear_data', label: '*reset*'  }) }
-    return (
-      <Dropdown value={selectedCell ? selectedCell.label : null} options={options} onChange={this.updateSelectedCell.bind(this)} placeholder="Cell?" />
-    )
-  }
-
-  // Could refactor these two dropdown functions into one
-  renderProjectDropDown() {
-    const { projects, selectedProject } = this.state
-    if(!projects) { return (<Dropdown disabled placeholder="Project" />) }
-    const options = projects.map((project) => {
-      return { value: project.id, label: project.name }
-    })
-    return (
-      <Dropdown value={selectedProject ? selectedProject.label : null} options={options} onChange={this.updateSelectedProject.bind(this)} placeholder="Project" />
-    )
-  }
-
-  updateSelectedCell(selection) {
-    this.setState({selectedCell: selection})
-  }
-
-  updateSelectedProject(selection) {
-    this.setState({selectedProject: selection})
-  }
-
   renderBody() {
     const { isLoggedIn, selectedCell } = this.state
     if(isLoggedIn && selectedCell) {
-      return (
-        <div className="Content" ref='view' />
-      )
+      return (<div className="Content" ref='view' />)
     } else {
-      return (
-        <img src={flux} className="App-logo" alt="logo" />
-      )
+      return (<img src={flux} className="App-logo" alt="logo" />)
     }
   }
 
